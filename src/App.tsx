3292f733fddc39a1218117c4ba72a4f94f95ec7d
Biraj Rajbhandari <birajr0@gmail.com>
2025-01-08 00:15:29 +0545

    feat: change font family to figtree
    - remove border right in sider
    - add figtree as main font for the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import CustomLayout from './components/CustomLayout/CustomLayout';
+import { ConfigProvider } from 'antd';
 
 const router = createBrowserRouter([
-  { element: <CustomLayout />,
+  { 
+    element: <CustomLayout />,
     children: [
       {
         path: '/',
         element: <div>contents</div>,
       },
-    ] },
+    ]
+  },
 ]);
 
 function App() {
   return (
-    <RouterProvider
-      router={router}
-    />
-
+    <ConfigProvider 
+      theme={{
+      token: {
+        fontFamily: "Figtree, serif"
+      }
+    }}>
+      <RouterProvider
+        router={router}
+      />
+    </ConfigProvider>
+    
   );
 }
 
