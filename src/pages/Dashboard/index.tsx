ab41eca57d225d2cd0494d9411c2b7fcf8941eb7
unknown <himerago042@gmail.com>
2025-01-15 17:02:22 +0545

    feat: update new design in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,13 @@
-import { ArrowRightOutlined, HomeOutlined } from '@ant-design/icons';
-import { Button, Card, Col, Flex, Row, Typography } from 'antd';
+import {
+  ArrowRightOutlined,
+  DropboxOutlined,
+  HomeOutlined,
+  ProductOutlined,
+  RollbackOutlined,
+} from '@ant-design/icons';
+import { Button, Card, Checkbox, Col, Flex, Row, Typography } from 'antd';
+
+import styles from './styles.module.css';
 
 const { Text } = Typography;
 
@@ -79,6 +87,149 @@ function Dashboard() {
             </Col>
           </Row>
         </Card>
+      </Col>
+      <Col span={24}>
+        <Row gutter={[16, 0]}>
+          <Col span={12}>
+            <Card>
+              <Row gutter={[0, 32]}>
+                <Col span={24}>
+                  <Flex vertical>
+                    <h1>Recent Notifications</h1>
+                    <Text type="secondary">
+                      You have 2 unread notifications
+                    </Text>
+                  </Flex>
+                </Col>
+                <Col span={24}>
+                  <Row gutter={[0, 16]}>
+                    <Col span={24}>
+                      <Flex vertical>
+                        <Checkbox>
+                          <h4> New Order from Admin.</h4>
+                        </Checkbox>
+                        <Text type="secondary" className={styles.date}>
+                          Oct, 17
+                        </Text>
+                      </Flex>
+                    </Col>
+                    <Col span={24}>
+                      <Flex vertical>
+                        <Checkbox>
+                          <h4> New Order from Admin.</h4>
+                        </Checkbox>
+                        <Text type="secondary" className={styles.date}>
+                          Oct, 17
+                        </Text>
+                      </Flex>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+          <Col span={12}>
+            <Card>
+              <Row gutter={[0, 32]}>
+                <Col span={24}>
+                  <Flex justify="space-between">
+                    <Flex vertical>
+                      <h1>New order</h1>
+                      <Text type="secondary">2+ New order</Text>
+                    </Flex>
+                    <Button
+                      type="link"
+                      icon={<ArrowRightOutlined />}
+                      iconPosition="end"
+                    >
+                      More
+                    </Button>
+                  </Flex>
+                </Col>
+                <Row gutter={[0, 16]}>
+                  <Col span={24}>
+                    <Flex vertical>
+                      <Checkbox>
+                        <h4> New Order from Admin.</h4>
+                      </Checkbox>
+                      <Text type="secondary" className={styles.date}>
+                        Oct, 17
+                      </Text>
+                    </Flex>
+                  </Col>
+                  <Col span={24}>
+                    <Flex vertical>
+                      <Checkbox>
+                        <h4> New Order from Admin.</h4>
+                      </Checkbox>
+                      <Text type="secondary" className={styles.date}>
+                        Oct, 17
+                      </Text>
+                    </Flex>
+                  </Col>
+                </Row>
+              </Row>
+            </Card>
+          </Col>
+        </Row>
+      </Col>
+      <Col span={24}>
+        <Row gutter={[32, 0]}>
+          <Col span={8}>
+            <Card>
+              <Flex justify="center" gap="small" vertical>
+                <Flex justify="center">
+                  <Text underline>11</Text>
+                </Flex>
+                <Flex justify="center">
+                  <ProductOutlined />
+                  <h2>Total Product</h2>
+                </Flex>
+                <Flex justify="center">
+                  <Text type="secondary">
+                    +2 new products added in the last 7 days.
+                  </Text>
+                </Flex>
+              </Flex>
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card>
+              <Flex justify="center" gap="small" vertical>
+                <Flex justify="center">
+                  <Text underline>2</Text>
+                </Flex>
+                <Flex justify="center">
+                  <DropboxOutlined />
+                  <h2>Total Order Value</h2>
+                </Flex>
+                <Flex justify="center">
+                  <Text type="secondary">
+                    Order value increased in the last 7 days.
+                  </Text>
+                </Flex>
+              </Flex>
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card>
+              <Flex justify="center" gap="small" vertical>
+                <Flex justify="center">
+                  <Text underline>2</Text>
+                </Flex>
+                <Flex justify="center">
+                  <RollbackOutlined />
+                  <h2>Total Return</h2>
+                </Flex>
+                <Flex justify="center">
+                  <Text type="secondary">
+                    Return decreased in the last 7 days.
+                  </Text>
+                </Flex>
+              </Flex>
+            </Card>
+          </Col>
+        </Row>
       </Col>
     </Row>
   );
